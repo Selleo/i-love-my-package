8fc331ef2ce7bb01d6992a4fc29621cbe80941b4
Monika Dziub-Korczynska <66638216+monika1127@users.noreply.github.com>
2023-01-20 14:46:04 +0100

    Details header (#16)
    * add details header styles
    
    * button wip
    
    * add header
    
    * add button

--- apps/web/src/Screens/Details/Details.tsx
+++ apps/web/src/Screens/Details/Details.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Tag from "../../components/Tag";
 import UsersList from "./UsersList/UsersList";
+import Button from "../../components/Button";
 
 function Details() {
   const { id } = useParams();
@@ -15,7 +16,7 @@ function Details() {
     reactions: {
       like: [
         { user: "Monika", comment: "some random comment" },
-        { user: "Monika" },
+        { user: "Monika", comment: "some random comment" },
         { user: "Monika", comment: "some random comment" },
       ],
       redFlag: [
@@ -32,10 +33,32 @@ function Details() {
   };
 
   const likeList = MOCK_DATA.reactions?.like || [];
+  const warningList = MOCK_DATA.reactions?.yellowFlag || [];
+  const dangerList = MOCK_DATA.reactions?.redFlag || [];
 
   const scrollToUsersSection = () => {};
 
-  return <UsersList variant="warning" list={MOCK_DATA.reactions.like} />;
+  return (
+    <div>
+      <div className="details__header">
+        <h1 className="details__title">{MOCK_DATA.name}</h1>
+        <Tag count={likeList.length} variant="like" />
+      </div>
+      <button
+        className="details__link"
+        onClick={scrollToUsersSection}
+      >{`(${MOCK_DATA.usedBy.length} people used)`}</button>
+      <div className="package-card__cards-wrapper">
+        <p className="package-card__paragraph -light-gray">Received Cards:</p>
+        <Tag count={1} variant="danger" /> <Tag count={3} variant="warning" />
+      </div>
+      <Button label="Add your reaction" />
+      <div className="divider" />
+      <UsersList variant="like" list={likeList} />
+      <UsersList variant="warning" list={warningList} />
+      <UsersList variant="danger" list={dangerList} />
+    </div>
+  );
 }
 
 export default Details;
--- apps/web/src/components/Tag/Tag.tsx
+++ apps/web/src/components/Tag/Tag.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { ReactComponent as CardIcon } from "../../assets/images/card.svg";
 import { ReactComponent as LikeIcon } from "../../assets/images/heart-filled.svg";
 
@@ -12,8 +14,11 @@ const Tag = ({ count, variant }: Props) => {
       <div className={`tag__mark -${variant}`}>
         {variant === "like" ? <LikeIcon /> : <CardIcon />}
       </div>
-
-      <p className="tag__description">{count}</p>
+      <p
+        className={`tag__description ${variant === "like" ? "" : "-underline"}`}
+      >
+        {count}
+      </p>
     </div>
   );
 };
