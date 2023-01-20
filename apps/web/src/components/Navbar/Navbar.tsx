eee572250709e4c804cd4fd0f5fd87210493dc1f
tomaszbilka <bila4@op.pl>
2023-01-20 12:53:38 +0100

    add navbar search input

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as Logo } from '../../assets/images/new.Selleo-logo.svg';
+import { ReactComponent as IconSearch } from '../../assets/images/Icon.svg';
 
 const Navbar = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchValue(e.target.value);
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLElement>) => {
+    if (e.keyCode === 13) {
+      console.log(searchValue);
+      //send request and redirect to search route
+      setSearchValue('');
+    }
+  };
+
   return (
     <nav className="navbar">
       <div>
         <Logo />
       </div>
-      <div>search</div>
+      <div>
+        <div className="navbar__input-wrapper">
+          <input
+            className="navbar__input"
+            placeholder="Search package"
+            value={searchValue}
+            onChange={handleChangeInput}
+            onKeyDown={handleSearch}
+          />
+          <IconSearch />
+        </div>
+      </div>
     </nav>
   );
 };
