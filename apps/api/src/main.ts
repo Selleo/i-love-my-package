e3aa79abfa1d697a987d902e6446579a8c2376d2
tomasz-zwak <t.zwak@selleo.com>
2023-01-20 14:40:43 +0100

    enable enableCors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import { UserGuard } from "./user/user.guard";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalGuards(new UserGuard());
   await app.listen(4000);
 }
