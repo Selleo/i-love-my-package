fefbe82779b3efd84ce332b8beb6f502de6e0c2c
Arek K <73478095+arekkonopka@users.noreply.github.com>
2023-01-20 13:50:51 +0100

    Search page (#12)
    * create search input
    
    * create package card and package search styles
    
    * complete package-card

--- apps/web/src/Screens/PackageSearch/index.tsx
+++ apps/web/src/Screens/PackageSearch/index.tsx
@@ -1,1 +1,31 @@
-export { default } from './PackageSearch';
+import { ChangeEvent, useState } from 'react'
+import { ReactComponent as IconSearch } from '../../assets/images/IconSearch.svg'
+import PackageCard from '../../components/PackageCard'
+
+function PackageSearch() {
+  const [searchValue, setSearchValue] = useState<string>()
+
+  const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
+  }
+
+  return (
+    <div className="package-search">
+      <div className="package-search__input-wrapper">
+        <input
+          className="package-search__input"
+          placeholder="Search package"
+          value={searchValue}
+          onChange={handleChangeInput}
+        />
+        <IconSearch />
+      </div>
+      <ul className="package-search__list">
+        <PackageCard />
+        <PackageCard />
+      </ul>
+    </div>
+  )
+}
+
+export default PackageSearch
--- apps/web/src/components/PackageCard/PackageCard.tsx
+++ apps/web/src/components/PackageCard/PackageCard.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom'
+import Tag from '../../components/Tag'
 
 const PackageCard = () => {
-  return <div>PackageCard</div>;
-};
+  const navigate = useNavigate()
 
-export default PackageCard;
+  const onClickCard = () => navigate('/details/1')
+
+  return (
+    <li className="package-card">
+      <div className="package-card__info-wrapper">
+        <div className="package-card__name-wrapper">
+          <button className="package-card__name" onClick={onClickCard}>
+            name
+          </button>
+          <button className="package-card__people">(24 people used)</button>
+        </div>
+        <div className="package-card__cards-wrapper">
+          <p className="package-card__paragraph -light-gray">Received Cards:</p>
+          <Tag count={1} variant="danger" /> <Tag count={3} variant="warning" />
+        </div>
+      </div>
+      <div className="package-card__likes-wrapper">
+        <Tag count={2} variant="like" />
+      </div>
+    </li>
+  )
+}
+
+export default PackageCard
